f8d5aecef64f17e44e1a9fec4e90fd50b37771f0
code-syl <ejjvandelaar@gmail.com>
2023-12-02 22:45:47 +0100

    Init - Day 1 pt 1 and 2 - recoverd from previous repository

---
+++ Day01/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+await Part1();
+await Part2();
+
+
+static async Task Part1()
+{
+    var lines = (await File.ReadAllLinesAsync("input.txt"));
+    
+    var sum =
+        lines.Select(line => line.First(char.IsDigit) + "" + line.Last(char.IsDigit))
+            .Select(firstAndLastDigit => Convert.ToInt32(firstAndLastDigit)).Sum();
+
+    Console.WriteLine("Part 1: " + sum);
+}
+
+
+static async Task Part2()
+{
+    var stringToNumbers = new Dictionary<string, int>
+    {
+        { "one", 1 }, { "two", 2 }, { "three", 3 }, { "four", 4 }, { "five", 5 }, { "six", 6 }, { "seven", 7 },
+        { "eight", 8 }, { "nine", 9 }, { "1", 1 }, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 },
+        { "7", 7 }, { "8", 8 }, { "9", 9 }
+    };
+    var lines = await File.ReadAllLinesAsync("input.txt");
+
+    var sum = lines.Select(line =>
+    {
+        var first = GetFirstAppearance(stringToNumbers, line);
+        var last = GetLastAppearance(stringToNumbers, line);
+        return Convert.ToInt32(first + last);
+    }).Sum();
+
+    Console.WriteLine("Part 2: " + sum);
+}
+
+static string GetFirstAppearance(Dictionary<string, int> dictionary, string input)
+{
+    return dictionary.Select(kvp => (Index: input.IndexOf(kvp.Key, StringComparison.Ordinal), Value: kvp.Value))
+                     .Where(ivp => ivp.Index >= 0)
+                     .MinBy(ivp => ivp.Index)
+                     .Value
+                     .ToString();
+    }
+
+static string GetLastAppearance(Dictionary<string, int> dictionary, string input)
+{
+    return dictionary.Select(kvp => (Index: input.LastIndexOf(kvp.Key, StringComparison.Ordinal), Value: kvp.Value))
+                     .Where(ivp => ivp.Index >= 0)
+                     .MaxBy(ivp => ivp.Index)
+                     .Value
+                     .ToString();
+}
+    
+    
