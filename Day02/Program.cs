0a2305f56b6019527591864ddc16f224de05f120
code-syl <ejjvandelaar@gmail.com>
2023-12-03 15:43:33 +0100

    Day2 part 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,17 +1,34 @@
 ﻿using Day02;
 
-await Part1();
+// parse input
+var lines = await File.ReadAllLinesAsync("input.txt");
+var games = lines.Select(GameFactory.CreateGame).ToList();
 
-static async Task Part1()
+// solve
+Part1(games);
+Part2(games);
+
+static void Part1(IEnumerable<Game> games)
 {
-    // parse input
-    var lines = await File.ReadAllLinesAsync("input.txt");
-    var games = lines.Select(GameFactory.CreateGame);
-    
     // which games are possible with only 12 red, 13 green, and 14 blue cubes
-    var possibleGames = 
-        games.Where(game => game.Sets.All(set => set is { Red: <= 12, Green: <= 13, Blue: <= 14 }));
-    var sumOfGameIds = possibleGames.Sum(game => game.Id);
+    var sum = games.Where(game => 
+        game.Sets.All(set => set is { Red: <= 12, Green: <= 13, Blue: <= 14 })
+    ).Sum(game => game.Id);
     
-    Console.WriteLine(sumOfGameIds);
+    Console.WriteLine(sum);
 }
+
+static void Part2(IEnumerable<Game> games)
+{
+    // get minimum needed for each color per game (for example, 1 red, 4 green, 3 blue)
+    // multiply those numbers with each other (for example 1*4*3=12)
+    // get the sum of these multiplications (for example 12+...)
+    var sum = games.Select(game =>
+    (
+        Red: game.Sets.Max(set => set.Red),
+        Green: game.Sets.Max(set => set.Green),
+        Blue: game.Sets.Max(set => set.Blue)
+    )).Select(colors => colors.Red * colors.Green * colors.Blue).Sum();
+
+    Console.WriteLine(sum);
+} 
