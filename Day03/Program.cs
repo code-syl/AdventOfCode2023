b0931757ca68a54f7bd1f911654c1a8b801e2ebf
code-syl <ejjvandelaar@gmail.com>
2023-12-05 10:06:14 +0100

    Day 3 pt 1 - needs refactoring

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,39 +1,65 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.Text;
 
 // parse input
 var lines = await File.ReadAllLinesAsync("input.txt");
 var grid = lines.Select(line => line.ToCharArray()).ToArray();
 
-//Part1(grid);
-var x = HasSurroundingSymbol(8, 0, grid);
-Console.WriteLine(x);
-
-// get 2d array of characters
-// loop through each character and check if it's a number
-// if it is a number, check its surroundings for symbols not in the invalidCharacters array
-    // if a symbol is found flag it
-    // continue through the rest of the number and add it to a list
+Part1(grid);
 
 static void Part1(char[][] grid)
 {
     var numberBuilder = new StringBuilder();
-    var numbersWithSymbolsAround = new List<int>();
+    var tempCoordinateSet = new List<Point>();
     
+    var numbers = new List<string>();
+    var coordinateSets = new List<List<Point>>();
+
     for (var y = 0; y < grid.Length; y++)
     {
+        numberBuilder.Clear();
+        tempCoordinateSet.Clear();
+
         for (var x = 0; x < grid[y].Length; x++)
         {
-            if (!char.IsDigit(grid[y][x])) continue;
-            var hasSurroundingSymbol = HasSurroundingSymbol(x, y, grid);
+            if (!char.IsDigit(grid[y][x]))
+            {
+                if (numberBuilder.Length > 0)
+                {
+                    numbers.Add(numberBuilder.ToString());
+                    coordinateSets.Add(new List<Point>(tempCoordinateSet));
+                }
+                numberBuilder.Clear();
+                tempCoordinateSet.Clear();
+                continue;
+            }
 
+            numberBuilder.Append(grid[y][x]);
+            tempCoordinateSet.Add(new Point(x, y));
+        }
+        
+        if (numberBuilder.Length > 0)
+        {
+            numbers.Add(numberBuilder.ToString());
+            coordinateSets.Add(new List<Point>(tempCoordinateSet));
         }
     }
+
+    var numbersWithSurroundingSymbol = new List<string>();
+    for (var i = 0; i < coordinateSets.Count; i++)
+    {
+        var hasSurroundingSymbol = coordinateSets[i].Any(point => HasSurroundingSymbol(point.X, point.Y, grid));
+        if (hasSurroundingSymbol) 
+            numbersWithSurroundingSymbol.Add(numbers[i]);
+    }
+
+    Console.WriteLine(numbersWithSurroundingSymbol.Sum(Convert.ToInt32));
+
 }
 
 static bool HasSurroundingSymbol(int centerX, int centerY, char[][] grid)
 {
     const char invalidSymbol = '.';
-    // select all surrounding coordinates from the x and y of grid
     
     var minX = Math.Max(0, centerX - 1);
     var maxX = Math.Min(grid[centerY].Length - 1, centerX + 1);
