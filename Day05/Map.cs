c1d9071cb49f6a69113819a45826badf983c292e
code-syl <ejjvandelaar@gmail.com>
2023-12-13 15:36:08 +0100

    Part 1

---
+++ Day05/Map.cs
@@ -0,0 +1,23 @@
+﻿namespace Day05;
+
+public class Map
+{
+    public string Label { get; set; }
+    public List<Set> Sets { get; set; }
+
+    public Map(string data)
+    {
+        var lines = data.Split("\r\n");
+        if (lines.Length <= 0)
+            throw new ArgumentException("The data is dirty.");
+
+        Label = lines[0];
+        Sets = lines[1..]
+            .Where(line => !string.IsNullOrWhiteSpace(line))
+            .Select(line => new Set(line))
+            .ToList();
+    }
+
+    public long Transform(long source) =>
+        Sets.FirstOrDefault(set => set.IsInRange(source))?.Transform(source) ?? source;
+}
