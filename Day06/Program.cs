7a7d702a4653d16cc1d1da168b19619fcb10ca35
code-syl <ejjvandelaar@gmail.com>
2023-12-14 12:25:31 +0100

    Refactor

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -3,34 +3,25 @@ using var streamReader = new StreamReader(input);
 
 var data = (await streamReader.ReadToEndAsync()).Split("\r\n");
 const StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
-var raceTimes = data[0].Split(':')[1].Split(' ', splitOptions).Select(int.Parse).ToList();
-var recordDistances = data[1].Split(':')[1].Split(' ', splitOptions).Select(int.Parse).ToList();
-var bigRaceTime = long.Parse(string.Join("", raceTimes.Select(t => t.ToString())));
-var bigRecordDistance = long.Parse(string.Join("", recordDistances.Select(t => t.ToString())));
+var raceTimes = data[0].Split(':')[1].Split(' ', splitOptions).Select(long.Parse).ToList();
+var recordDistances = data[1].Split(':')[1].Split(' ', splitOptions).Select(long.Parse).ToList();
+var bigRaceTime = long.Parse(string.Join("", raceTimes.Select(rt => rt)));
+var bigRecordDistance = long.Parse(string.Join("", recordDistances.Select(rd => rd)));
 
 Console.WriteLine($"Part 1: {Part1(raceTimes, recordDistances)}");
 Console.WriteLine($"Part 2: {Part2(bigRaceTime, bigRecordDistance)}");
 return;
 
-static int Part1(IReadOnlyList<int> raceTimes, IReadOnlyList<int> recordDistances)
+static long Part1(IReadOnlyList<long> raceTimes, IReadOnlyList<long> recordDistances)
 {
     // for each millisecond spent pushing the button, the boat gains a distance of that time in millimeters.
-    var possibleWinningTimes = new List<int>();
+    var possibleWinningTimes = new List<long>();
     for (var race = 0; race < raceTimes.Count; race++)
     {
-        var numberOfWinningTimesForThisRace = 0;
         var raceTime = raceTimes[race];
         var recordDistance = recordDistances[race];
-
-        for (var ms = 0; ms <= raceTime; ms++)
-        {
-            var timeLeft = raceTime - ms;
-            var distance = timeLeft * ms;
-            if (distance > recordDistance)
-                numberOfWinningTimesForThisRace++;
-        }
         
-        possibleWinningTimes.Add(numberOfWinningTimesForThisRace);
+        possibleWinningTimes.Add(GetWinningTimes(raceTime, recordDistance));
     }
 
     return possibleWinningTimes.Aggregate((value, next) => value * next);
@@ -38,14 +29,19 @@ static int Part1(IReadOnlyList<int> raceTimes, IReadOnlyList<int> recordDistance
 
 static long Part2(long bigRaceTime, long bigRecordDistance)
 {
-    long possibleWinningTimes = 0;
-    for (var ms = 0; ms < bigRaceTime; ms++)
+    return GetWinningTimes(bigRaceTime, bigRecordDistance);
+}
+
+static long GetWinningTimes(long raceTime, long recordDistance)
+{
+    var winningTimes = 0;
+    for (var ms = 0; ms < raceTime; ms++)
     {
-        var timeLeft = bigRaceTime - ms;
+        var timeLeft = raceTime - ms;
         var distance = timeLeft * ms;
-        if (distance > bigRecordDistance)
-            possibleWinningTimes++;
+        if (distance > recordDistance)
+            winningTimes++;
     }
-    
-    return possibleWinningTimes;
+
+    return winningTimes;
 }
