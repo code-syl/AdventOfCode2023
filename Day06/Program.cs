c4ecc6c1bf35fdd4924391b15df6008105a596ba
code-syl <ejjvandelaar@gmail.com>
2023-12-14 12:11:37 +0100

    Day 6 Part 2

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -5,8 +5,11 @@ var data = (await streamReader.ReadToEndAsync()).Split("\r\n");
 const StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
 var raceTimes = data[0].Split(':')[1].Split(' ', splitOptions).Select(int.Parse).ToList();
 var recordDistances = data[1].Split(':')[1].Split(' ', splitOptions).Select(int.Parse).ToList();
+var bigRaceTime = long.Parse(string.Join("", raceTimes.Select(t => t.ToString())));
+var bigRecordDistance = long.Parse(string.Join("", recordDistances.Select(t => t.ToString())));
 
 Console.WriteLine($"Part 1: {Part1(raceTimes, recordDistances)}");
+Console.WriteLine($"Part 2: {Part2(bigRaceTime, bigRecordDistance)}");
 return;
 
 static int Part1(IReadOnlyList<int> raceTimes, IReadOnlyList<int> recordDistances)
@@ -31,4 +34,18 @@ static int Part1(IReadOnlyList<int> raceTimes, IReadOnlyList<int> recordDistance
     }
 
     return possibleWinningTimes.Aggregate((value, next) => value * next);
+}
+
+static long Part2(long bigRaceTime, long bigRecordDistance)
+{
+    long possibleWinningTimes = 0;
+    for (var ms = 0; ms < bigRaceTime; ms++)
+    {
+        var timeLeft = bigRaceTime - ms;
+        var distance = timeLeft * ms;
+        if (distance > bigRecordDistance)
+            possibleWinningTimes++;
+    }
+    
+    return possibleWinningTimes;
 }
